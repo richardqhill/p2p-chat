eff70780a025cd73db1267310692e65ae1635550
Richard Hill <richardqhill@gmail.com>
2018-11-17 13:45:48 -0500

    Can send over UDP to all neighbors

--- main.cc
+++ main.cc
@@ -1,126 +1,134 @@
-
 #include <unistd.h>
-
 #include <QVBoxLayout>
 #include <QApplication>
 #include <QDebug>
 #include <QFile>
 #include <QDataStream>
-
+#include <string>
 #include "main.hh"
 
 
-int myPort, portMin, portMax;
-
 ChatDialog::ChatDialog(){
-	setWindowTitle("P2Papp");
 
-	// Read-only text box where we display messages from everyone.
-	// This widget expands both horizontally and vertically.
+    mySocket = new NetSocket();
+    if(!  mySocket->bind()) {
+		exit(1);
+	}
+    else {
+		myPort = mySocket->getPort();
+	}
+
+	mySeqNo = 0;
+
+    setWindowTitle("P2Papp");
+
 	textview = new QTextEdit(this);
 	textview->setReadOnly(true);
-
-	// Small text-entry box the user can enter messages.
-	// This widget normally expands only horizontally,
-	// leaving extra vertical space for the textview widget.
-	//
-	// You might change this into a read/write QTextEdit,
-	// so that the user can easily enter multi-line messages.
 	textline = new QLineEdit(this);
-
-	// Lay out the widgets to appear in the main window.
-	// For Qt widget and layout concepts see:
-	// http://doc.qt.nokia.com/4.7-snapshot/widgets-and-layouts.html
 	QVBoxLayout *layout = new QVBoxLayout();
 	layout->addWidget(textview);
 	layout->addWidget(textline);
 	setLayout(layout);
 
-	// Register a callback when another p2papp sends us a message over
-	// UDP
-	//connect(&QUdpSocket, SIGNAL(readyRead()),
-	//		this, SLOT(processPendingDatagrams()));
-
 	// Register a callback on the textline's returnPressed signal
-	// so that we can send the message entered by the user.
 	connect(textline, SIGNAL(returnPressed()),
 		this, SLOT(gotReturnPressed()));
-}
 
-void ChatDialog::processPendingDatagrams(){
-
-	;
+    // Register a callback when another p2papp sends us a message over UDP
+    connect(mySocket, SIGNAL(readyRead()),
+    		this, SLOT(processPendingDatagrams()));
 }
 
 void ChatDialog::gotReturnPressed(){
 
-    QString msg = textline->text();
-    textview->append(textline->text());
+	textview->append(QString(myPort)); // prints out chinese character, need to cast?
+	textview->append(textline->text());
 
-    // Clear the text line to get ready for the next input message.
-    textline->clear();
-
-    QMap<QString, QString> outMap, inMap;
-    QByteArray mapData;
-
-    outMap.insert("ChatText", msg);
-
-    QDataStream outStream(&mapData, QIODevice::WriteOnly);
-    outStream << outMap;
-    qDebug() << outMap;
-
-    QDataStream inStream(&mapData, QIODevice::ReadOnly);
-    inStream >> inMap;
-    qDebug() << inMap;
+	QString msg = textline->text();
+	outgoing_messages.insert("ChatText", msg);
 
 
 
-    qDebug() << "FIX: send message to other peers: " << inMap["ChatText"];
 
-    qDebug() << "My port is: " << myPort;
 
-    // To do: do not send to yourself and to the person the message came from
-	for(quint16 port=portMin; port<= portMax && port!=myPort; port++)
-	{
-		; //QUdpSocket::writeDatagram(&mapData, QHostAddress::LocalHost, port);
 
+    //serialize
+    QByteArray outData;
+    QDataStream outStream(&outData, QIODevice::WriteOnly); //Constructs a data stream that operates on a byte array
+    outStream << outgoing_messages;
+
+    for(int i = mySocket->myPortMin; i<= mySocket->myPortMax; i++){
+        mySocket->writeDatagram(outData.data(), outData.size(), QHostAddress::LocalHost, i);
+    }
+
+
+	//createRumorMessage(QString(myPort), quint16(mySeqNo));
+
+
+	mySeqNo += 1;
+
+	// Clear the text line to get ready for the next input message.
+	textline->clear();
+}
+
+
+void ChatDialog::processPendingDatagrams(){
+
+	while(mySocket->hasPendingDatagrams()){
+		QByteArray datagram;
+		datagram.resize(mySocket->pendingDatagramSize());
+        QHostAddress sender;
+        quint16 senderPort;
+
+		if(mySocket->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort) != -1)
+			deserializeMessage(datagram);
+		else
+			return;
 	}
-    // send to every port b/w 32768 to 49151
+}
 
-    //QUdpSocket::writeDatagram()
+void ChatDialog::deserializeMessage(QByteArray datagram) {
 
+	QDataStream inStream(&datagram, QIODevice::ReadOnly);
+    QMap<QString, QString> inMap;
+	inStream >> inMap;
+
+    qDebug() << inMap;
 
 }
 
+
+
+
+
+
+
+
+/* Pick a range of four UDP ports to try to allocate by default, computed based on my Unix user ID.*/
 NetSocket::NetSocket(){
-	// Pick a range of four UDP ports to try to allocate by default,
-	// computed based on my Unix user ID.
-	// This makes it trivial for up to four P2Papp instances per user
-	// to find each other on the same host,
-	// barring UDP port conflicts with other applications
-	// (which are quite possible).
-	// We use the range from 32768 to 49151 for this purpose.
+	// This makes it trivial for up to four P2Papp instances per user to find each other on the same host,
+	// barring UDP port conflicts with other applications (which are quite possible). We use the range from
+	// 32768 to 49151 for this purpose.
 	myPortMin = 32768 + (getuid() % 4096)*4;
 	myPortMax = myPortMin + 3;
 
-	portMin = myPortMin;
-	portMax = myPortMax;
 }
 
+/* Bind one of ports between myPortMin and myPortMax */
 bool NetSocket::bind(){
-	// Try to bind to each of the range myPortMin..myPortMax in turn.
 	for (int p = myPortMin; p <= myPortMax; p++) {
 		if (QUdpSocket::bind(p)) {
 			qDebug() << "bound to UDP port " << p;
-			myPort = p;
-
+			port = p;
 			return true;
 		}
 	}
-
-	qDebug() << "Oops, no ports in my default range " << myPortMin
-		<< "-" << myPortMax << " available";
+	qDebug() << "Oops, no ports in my default range " << myPortMin << "-" << myPortMax << " available";
 	return false;
+}
+
+int NetSocket::getPort(){
+    return port;
 }
 
 int main(int argc, char **argv){
@@ -130,11 +138,6 @@ int main(int argc, char **argv){
 	// Create an initial chat dialog window
 	ChatDialog dialog;
 	dialog.show();
-
-	// Create a UDP network socket
-	NetSocket sock;
-	if (!sock.bind())
-		exit(1);
 
 	// Enter the Qt main loop; everything else is event driven
 	return app.exec();
--- main.hh
+++ main.hh
@@ -6,12 +6,35 @@
 #include <QLineEdit>
 #include <QUdpSocket>
 
+
+class NetSocket : public QUdpSocket
+{
+    Q_OBJECT
+
+public:
+
+    NetSocket();
+
+    // Bind this socket to a P2Papp-specific default port.
+    bool bind();
+
+    int getPort();
+    int myPortMin, myPortMax, port;
+
+private:
+
+};
+
+
+
 class ChatDialog : public QDialog
 {
 	Q_OBJECT
 
 public:
-	ChatDialog();
+    ChatDialog();
+
+    NetSocket* mySocket;
 
 public slots:
 	void gotReturnPressed();
@@ -20,20 +43,21 @@ public slots:
 private:
 	QTextEdit *textview;
 	QLineEdit *textline;
+
+    quint16 myPort;
+    quint16 mySeqNo;
+
+    QMap <QString, QString> outgoing_messages;
+
+
+    void serializeMessage(QVariantMap &myMap);
+    void deserializeMessage(QByteArray datagram);
+
+
 };
 
-class NetSocket : public QUdpSocket
-{
-	Q_OBJECT
 
-public:
-	NetSocket();
 
-	// Bind this socket to a P2Papp-specific default port.
-	bool bind();
 
-private:
-	int myPortMin, myPortMax;
-};
 
 #endif // P2PAPP_MAIN_HH
